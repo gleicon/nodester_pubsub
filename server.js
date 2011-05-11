961169caed8ee7b9f56f287b6e2d2ab1e16f5a6f
Gleicon Moraes <gleicon@gmail.com>
2011-05-10 23:48:17 -0300

    pub/sub com uri

--- server.js
+++ server.js
@@ -4,22 +4,23 @@
 var sys = require("sys");
 var http = require("http")
 var qs = require("querystring");
+var url = require("url")
+
 
 process.on('uncaughtException', function (err) {
 	console.log('exception: ' + err);
 });
 
 
-var e_msg = new process.EventEmitter();
 var users = 0;
-
-
-setTimeout(connected_users, 1000);
+var presence = new Array();
 
 function connected_users() {
   console.log('Connected users: '+users);
   setTimeout(connected_users, 30 * 1000);
 }
+
+setTimeout(connected_users, 1000);
 
 
 console.log('Initializing COMET pub/sub server');
@@ -29,35 +30,49 @@ server = http.createServer(function (req, res) {
         res.write(m);
         res.write('\n');
       }
-      if (req.url == '/subscribe') {
-        req.connection.setTimeout(0);
-        res.writeHead(200, {'Content-type':'text/plain'});
-        e_msg.addListener('message', l);
 
-      } else if (req.url == '/publish') {
-				var m ="";
-        req.on('data', function(d) {
-          m = m + d 
-					console.log(d);
-        });
-				req.on('end', function(){
-					params = qs.parse(m);
-					m = params['body'];
-					if (m != null) e_msg.emit('message', m); 
-				});
+			path = url.parse(req.url).pathname
+			switch(req.method) {
 				
-        res.writeHead(200, {'Content-type':'text/plain'});
-        res.write('ok\n');
-        res.end();
-      } else {
-        res.writeHead(404, {'Content-type':'text/plain'});
-        res.write('not found');
-        res.end();
-      }
+				case 'GET':
+					req.connection.setTimeout(0);
+				  res.writeHead(200, {'Content-type':'text/plain'});
+				  if (presence[path] == null)	presence[path] = new process.EventEmitter();
+					presence[path].addListener('message', l);					
+					break
+					
+				case 'POST':
+					var m ="";
+	        req.on('data', function(d) {
+	          m = m + d 
+	        });
+					req.on('end', function(){
+						params = qs.parse(m);
+						m = params['body'];
+						if (m != null) {
+							if (presence[path] == null) presence[path] = new process.EventEmitter()
+							presence[path].emit('message', m);
+						}
+					});		
+					res.writeHead(200, {'Content-type':'text/plain'});
+	        res.write('ok\n');
+	        res.end();
+	        
+					break
+				
+				default:
+					res.writeHead(401, {'Content-type':'text/plain'});
+	        res.write('Method not allowed');
+	        res.end();
+					break
+			}
+			
+				
 });
 
 server.maxConnections = 10000;
-server.on('request', function() { console.log('conn'); users++; });
+server.on('connection', function(s) { console.log('conn: ' + s.address()); users++; });
+server.on('close', function() {if (users > 0) users--; }); 
 server.on('clientError', function() {if (users > 0) users--; }); 
 
 server.listen(9849);
